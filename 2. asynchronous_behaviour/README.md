5b230595e13b55686898028512dfc10d2550b5c3
saikatnoman12 <noommaa240@gmail.com>
2022-09-29 10:55:49 +0600

    add js asynchronous behaviour

---
+++ 2. asynchronous_behaviour/README.md
@@ -0,0 +1,12 @@
+# JavaScript Asynchronous behaviour
+
+### What is js asynchronous behaviour
+
+Ans: The JavaScript event loop executes all synchronous code first.
+Then the JavaScript waits for the timer to complete. Then when the timer completes. Then
+JavaScript calls asynchronous methods.
+
+### Asynchronous method
+    setTimeOut(callBackFunction, timer);
+
+    setInterval(callBackFunction, timer); 
---
+++ 2. asynchronous_behaviour/async.js
@@ -0,0 +1,24 @@
+/* === JavaScript asynchronous ==== */
+
+
+// js synchronous method 
+console.log('Hello my 1st friend.');
+
+// js asynchronous method  
+setTimeout(function () {
+    console.log('Hello my 2nd friend.');
+}, 2000);
+
+// js synchronous method 
+console.log('Hello my 3rd friend');
+
+// js asynchronous method
+setInterval(function(){
+    console.log('Hello my 4th friend.');
+}, 3000)
+
+// js synchronous method; 
+console.log('Hello my 5th friend');
+
+
+
