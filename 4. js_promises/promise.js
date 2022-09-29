a52528e11e6bbff28bd5619dc440e85da984ae4e
saikatnoman12 <noommaa240@gmail.com>
2022-09-29 22:57:21 +0600

    add promise example

--- 4. js_promises/promise.js
+++ 4. js_promises/promise.js
@@ -8,29 +8,29 @@ console.log('js 2');
 const paymentStatus = true;
 
 // promise definition
-// const promise = new Promise(function (resolve, reject) {
-//     setTimeout(function () {
-//         if (paymentStatus) {
-//             resolve('Your payment process is success. js 3.');
-//         }
-//         else {
-//             reject('Your payment process is failed.');
-//         }
-//     }, 4000);
+const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        if (paymentStatus) {
+            resolve('Your payment process is success. js 3.');
+        }
+        else {
+            reject('Your payment process is failed.');
+        }
+    }, 4000);
 // });
 
 
 // call promise
-// promise
-//     .then(function (value) {
-//         console.log(value);
-//     })
-//     .catch(function (value) {
-//         console.log(value);
-//     });
+promise
+    .then(function (value) {
+        console.log(value);
+    })
+    .catch(function (value) {
+        console.log(value);
+    });
 
 
-// console.log('js 4');
+console.log('js 4');
 
 
 
